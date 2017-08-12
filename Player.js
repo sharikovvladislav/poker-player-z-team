bee19d62fdfde80642d84db468084b177c72056d
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-12 11:25:16 +0300

    remove 2x logs

--- Player.js
+++ Player.js
@@ -12,17 +12,12 @@ class Player {
     console.log('gameState.bet', gameState.bet);
     console.log('gameState.minimum_raise', gameState.minimum_raise);
     console.log('gameState.players', gameState.players);
-    
+
     var myBet =
       gameState.current_buy_in -
       gameState.players[gameState.in_action][gameState.bet] +
       gameState.minimum_raise +
       1;
-    console.log('gameState.current_buy_in', gameState.current_buy_in);
-    console.log('gameState.in_action', gameState.in_action);
-    console.log('gameState.bet', gameState.bet);
-    console.log('gameState.minimum_raise', gameState.minimum_raise);
-    console.log('gameState.players', gameState.players);
     console.log('my bet:', myBet);
     bet(0);
   }
