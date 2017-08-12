d6a7e9a9212069e8559ef9ebd9964ed4fa9721ee
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-12 11:22:06 +0300

    add logs

--- Player.js
+++ Player.js
@@ -7,6 +7,23 @@ class Player {
     // current_buy_in - players[in_action][bet] + minimum_raise
     console.log(gameState);
 
+    console.log('gameState.current_buy_in', gameState.current_buy_in);
+    console.log('gameState.in_action', gameState.in_action);
+    console.log('gameState.bet', gameState.bet);
+    console.log('gameState.minimum_raise', gameState.minimum_raise);
+    console.log('gameState.players', gameState.players);
+    
+    var myBet =
+      gameState.current_buy_in -
+      gameState.players[gameState.in_action][gameState.bet] +
+      gameState.minimum_raise +
+      1;
+    console.log('gameState.current_buy_in', gameState.current_buy_in);
+    console.log('gameState.in_action', gameState.in_action);
+    console.log('gameState.bet', gameState.bet);
+    console.log('gameState.minimum_raise', gameState.minimum_raise);
+    console.log('gameState.players', gameState.players);
+    console.log('my bet:', myBet);
     bet(0);
   }
 
