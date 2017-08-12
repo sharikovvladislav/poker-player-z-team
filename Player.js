f404f234a56ef8e82fc3e8e38ec7878cae2d9ac3
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-12 11:12:11 +0300

    add console.log gameState

--- Player.js
+++ Player.js
@@ -5,6 +5,7 @@ class Player {
 
   static betRequest(gameState, bet) {
     // current_buy_in - players[in_action][bet] + minimum_raise
+    console.log(gameState);
     var bet =
       gameState.current_buy_in -
       gameState.players[gameState.in_action][gameState.bet];
