10cb626de9f28da6a3c866083b5405743549504a
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-12 12:27:52 +0300

    fix suts

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 module.exports = {
-  suits: ['D', 'C', 'H', 'S'],
+  suits: ['diamonds', 'clubs', 'hearts', 'spades'],
   ranks: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 };
