8c7f99c0d158880f0abbcc991375be8cbcfbacca
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-12 11:21:17 +0300

    bet 0

--- Player.js
+++ Player.js
@@ -6,13 +6,8 @@ class Player {
   static betRequest(gameState, bet) {
     // current_buy_in - players[in_action][bet] + minimum_raise
     console.log(gameState);
-    var myBet =
-      gameState.current_buy_in -
-      gameState.players[gameState.in_action][gameState.bet] +
-      gameState.minimum_raise +
-      1;
-    console.log('my bet:', myBet);
-    bet(myBet);
+
+    bet(0);
   }
 
   static showdown(gameState) {}
