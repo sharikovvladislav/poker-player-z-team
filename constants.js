419d272d43f66a31371a6f5e69e1be7fd7b8aa0a
yuriy.sedinkin <yuriy.sedinkin@billing.ru>
2017-08-12 12:57:02 +0300

    constants

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
 module.exports = {
   suits: ['diamonds', 'clubs', 'hearts', 'spades'],
+  suitsShort: ['D', 'C', 'H', 'S'],
   ranks: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 };
