29def57e6b1d840e43691e589c6e0b09167ed9eb
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-12 11:35:28 +0300

    try catch

--- Player.js
+++ Player.js
@@ -5,21 +5,26 @@ class Player {
 
   static betRequest(gameState, bet) {
     // current_buy_in - players[in_action][bet] + minimum_raise
-    console.log(gameState);
+    try {
+      console.log(gameState);
 
-    console.log('gameState.current_buy_in', gameState.current_buy_in);
-    console.log('gameState.in_action', gameState.in_action);
-    console.log('gameState.bet', gameState.bet);
-    console.log('gameState.minimum_raise', gameState.minimum_raise);
-    console.log('gameState.players', gameState.players);
+      console.log('gameState.current_buy_in', gameState.current_buy_in);
+      console.log('gameState.in_action', gameState.in_action);
+      console.log('gameState.bet', gameState.bet);
+      console.log('gameState.minimum_raise', gameState.minimum_raise);
+      console.log('gameState.players', gameState.players);
 
-    var myBet =
-      gameState.current_buy_in -
-      gameState.players[gameState.in_action].bet +
-      gameState.minimum_raise +
-      1;
-    console.log('my bet:', myBet);
-    bet(myBet);
+      var myBet =
+        gameState.current_buy_in -
+        gameState.players[gameState.in_action].bet +
+        gameState.minimum_raise +
+        1;
+      console.log('my bet:', myBet);
+      bet(myBet);
+    } catch (e) {
+      console.log(e);
+      bet(0);
+    }
   }
 
   static showdown(gameState) {}
