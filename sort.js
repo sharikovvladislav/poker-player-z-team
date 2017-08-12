2d21c7a8aadceb68d62f3ab4c1139b82b6b6382e
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-08-12 14:06:41 +0300

    fix sort (not mutating)

--- sort.js
+++ sort.js
@@ -1,7 +1,7 @@
 const constants = require('./constants');
 
 function sort(cards) {
-  const sortedCards = cards;
+  const sortedCards = [...cards];
 
   sortedCards.sort((a, b) => {
     return constants.ranks.indexOf(b.rank) - constants.ranks.indexOf(a.rank);
